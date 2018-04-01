5c63a390f7dc08f16a1a3e7a7795b17bad36d23b
Ben Newsome <ben@newsome.me.uk>
2018-04-01 22:54:18 +0100

    Changed from steam scripts to steam commands for greater security and not temporarly storing plain text credentials.

--- tests/steam-wrapper-test.js
+++ tests/steam-wrapper-test.js
@@ -15,12 +15,9 @@ describe('ReadUserCredentials', function() {
 describe('CreateSteamRunScriptText', function() {
     it('createSteamRunScriptText(args) should return a string if all the correct args are passed in', function() {
         var script_args = {"user": "user", 'password': 'password', 'platform': 'linux', 'install_location': '~/steam_temp', 'app_id': '271590'};
-        var expected_output = 
-            "@ShutdownOnFailedCommand 1\n" +
-            "@NoPromptForPassword 1\n" +
-            "@sSteamCmdForcePlatformType linux\n" +
-            "login user password\n" +
-            "force_install_dir ~/steam_temp\n";
+        var expected_output =
+            "steamcmd +@sSteamCmdForcePlatformType linux +login user password " + 
+            "+force_install_dir ~/steam_temp +app_update 346900 validate +quit"
         var steamWrapper = new SteamWrapper();
         steamRunScriptText = steamWrapper.createSteamRunScriptText(script_args);
         expect(steamRunScriptText).to.eql(expected_output);
@@ -31,5 +28,12 @@ describe('CreateSteamRunScriptText', function() {
         expect(() => steamWrapper.createSteamRunScriptText(script_args) ).to.throw( Error );
     });
 });
+
+
+
+// A good small free to play test game
+// Name: Adventure Cpaitalist
+// ID: 346900
+
 
 
